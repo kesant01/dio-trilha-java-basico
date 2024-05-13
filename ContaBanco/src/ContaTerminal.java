7f9b9b796a8822ac8bd8d1498a9afe9293f409d5
Ketlin <ketlinpereiraa@outlook.com>
2024-05-13 20:17:02 -0300

    Commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Olá! \nPor favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o valor do saldo a ser depositado: ");
+        Double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + "! \nObrigado por criar uma conta em nosso banco. " +
+                "\nSua agência é " + agencia + ", sua conta é " + numero + " e" +
+                "\nseu saldo de R$" + saldo + " já está disponível para saque." );
+    }
+}
